93844bcb0fa63371562ff2e779e480749d90147f
Ceobe33 <youareaaron0@gmail.com>
2026-08-24 10:37:33 +0800

    update

--- plugin/coding/urlview.lua
+++ plugin/coding/urlview.lua
@@ -1,6 +1,8 @@
-require('urlview').setup({
+local exist, mod = pcall(require, 'urlview')
+if exist then
+  require('urlview').setup({
+  })
 
-})
-
-Map("n", "\\u", "<Cmd>UrlView<CR>", { desc = "View buffer URLs" })
-Map("n", "\\U", "<Cmd>UrlView packer<CR>", { desc = "View Packer plugin URLs" })
+  Map("n", "\\u", "<Cmd>UrlView<CR>", { desc = "View buffer URLs" })
+  Map("n", "\\U", "<Cmd>UrlView packer<CR>", { desc = "View Packer plugin URLs" })
+end
